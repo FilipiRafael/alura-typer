0d28571587963401524aa4a254823bdd81414037
Filipi Rafael <filipirafael.123@gmail.com>
2022-07-11 13:23:14 -0300

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,51 @@
-const frase = $('.frase').text();
-
-const numeroDePalavras = frase.split(/\S+/).length;
-
-const tamanhoDaFrase = $('#tamanhoDaFrase');
-tamanhoDaFrase.text(numeroDePalavras);
+$(document).ready(() => {
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $('#botao-reiniciar').on('click', reiniciaJogo);
+})
 
 const campo = $('.campo-digitacao');
-campo.on('input', () => {
-    const conteudo = campo.val();
-    const quantidadePalavras = conteudo.split(/\S+/).length;
-    $('#contador-palavras').text(quantidadePalavras);
 
-    const quantidadeCaracteres = conteudo.length;
-    $('#contador-caracteres').text(quantidadeCaracteres);
-});
+function atualizaTamanhoFrase() {
+    const frase = $('.frase').text();
+    const numeroDePalavras = frase.split(/\S+/).length;
+    const tamanhoDaFrase = $('#tamanhoDaFrase');
+    tamanhoDaFrase.text(numeroDePalavras);
+}
 
-let tempoRestante = $('#tempo-digitacao').text();
-campo.one('focus', () => {
-    setInterval(() => {
-        if (tempoRestante > 0) {
-            tempoRestante--;
-            $('#tempo-digitacao').text(tempoRestante);
-            if (tempoRestante < 1) {
-                campo.attr('disabled', true);
+
+function inicializaContadores() {
+    campo.on('input', () => {
+        const conteudo = campo.val();
+        const quantidadePalavras = conteudo.split(/\S+/).length;
+        $('#contador-palavras').text(quantidadePalavras);
+    
+        const quantidadeCaracteres = conteudo.length;
+        $('#contador-caracteres').text(quantidadeCaracteres);
+    });
+}
+
+function inicializaCronometro() {
+    let tempoRestante = $('#tempo-digitacao').text();
+    campo.one('focus', () => {
+        setInterval(() => {
+            if (tempoRestante > 0) {
+                tempoRestante--;
+                $('#tempo-digitacao').text(tempoRestante);
+                if (tempoRestante < 1) {
+                    campo.attr('disabled', true);
+                }
             }
-        }
-    }, 1000);
-});
+        }, 1000);
+    });
+}
+
+function reiniciaJogo() {
+    campo.attr('disabled', false);
+    campo.val('');
+    $('#tempo-digitacao').text('10');
+    $('#contador-palavras').text('0');
+    $('#contador-caracteres').text('0');
+    inicializaCronometro();
+}
